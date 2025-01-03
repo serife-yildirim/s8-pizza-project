3230d6a9a9b764d48c883716ba0780f4b2a07e45
serife-yildirim <serifeozyurek-94@hotmail.com>
2025-01-03 12:55:40 +0300

    Initialize project structure and create App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-export default function App() {
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { OrderProvider } from "./context/OrderContext";
+import Main from "./components/Main";
+import SiparisFormu from "./components/SiparisFormu";
+import SiparisOnayi from "./components/SiparisOnayi";
+import { Header } from "./components/Header";
+
+const App = () => {
   return (
-    <div className="bg-blue-500 text-white text-center p-4">
-      Tailwind CSS Çalışıyor! 🎉
-    </div>
+    <OrderProvider>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route path="/siparis-formu" component={SiparisFormu} />
+          <Route path="/siparis-onayi" component={SiparisOnayi} />
+        </Switch>
+      </BrowserRouter>
+    </OrderProvider>
   );
-}
+};
+
+export default App;
